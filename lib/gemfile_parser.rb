46255ef3f7710fe15dea2d61630a461a7b6c929b
Shinsuke Nishio <nishio@densan-labs.net>
2015-10-12 06:43:56 +0000

    Gemspec parser作成

--- lib/gemfile_parser.rb
+++ lib/gemfile_parser.rb
@@ -1,14 +1,15 @@
 require 'tempfile'
 
-# [使い方]
-#   GemfileParser.new.parse(rubygems_contents)
-#
-# [戻り値]
-#   Array
-#     - [0] boolean - パースに成功したか
-#     - [1] array - Gem Dependenciesのリスト
-#     - [2] object - エラークラス
 class GemfileParser
+
+  # [使い方]
+  #   GemfileParser.new.parse_gemfile(rubygems_contents)
+  #
+  # [戻り値]
+  #   Array
+  #     - [0] boolean - パースに成功したか
+  #     - [1] array - Gem Dependenciesのリスト
+  #     - [2] object - エラークラス
   def parse_gemfile(rubygems_contents)
     gem_lines = rubygems_contents
       .split("\n")
@@ -32,5 +33,28 @@ class GemfileParser
       file.unlink
     end
     [is_success, gems, error]
+  end
+
+  # Gemspecの中からadd_dependency, add_development_dependencyの行だけ抽出
+  # そこからgemfile名を推測する
+  #
+  # [使い方]
+  #   GemfileParser.new.parse_gemspec(contents)
+  #
+  # [戻り値]
+  #   Array - Gemfile名のリスト
+  def parse_gemspec(gemspec_contents)
+    dependencies = gemspec_contents
+      .split("\n")
+      .select { |v| v.include?("add_dependency") }
+      .map { |v| v.scan(/add_dependency\s+(.[^,)]*)/).flatten.first }
+      .map { |v| v.gsub("'","").gsub("\"","").gsub("(", "").gsub(")", "") }
+    dev_dependencies = gemspec_contents
+      .split("\n")
+      .select { |v| v.include?("add_development_dependency") }
+      .map { |v| v.scan(/add_development_dependency\s+(.[^,)]*)/).flatten.first }
+      .map { |v| v.gsub("'","").gsub("\"","").gsub("(", "").gsub(")", "") }
+
+    dependencies.concat(dev_dependencies)
   end
 end
