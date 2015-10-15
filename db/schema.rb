ab5dc58e4ce545000e19ff215997e830b3fa7450
r_nishio <quick.xp@me.com>
2015-10-16 01:30:55 +0900

    rubygems 初回情報格納ツール作成

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 0) do
   create_table "input_libraries", force: :cascade do |t|
     t.integer  "input_project_id", limit: 4
     t.string   "name",             limit: 255, null: false
-    t.string   "version",          limit: 255, null: false
+    t.string   "version",          limit: 255
     t.string   "homepage_uri",     limit: 255
     t.string   "source_code_uri",  limit: 255
     t.datetime "created_at",                   null: false
