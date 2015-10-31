670204dd41529a0727960a0fa12a2c95b7033437
r_nishio <quick.xp@me.com>
2015-10-31 20:35:02 +0900

    Project Detail Crawler size取得, 詳細情報取得失敗時にステータスをエラーに更新するように修正

--- app/jobs/github_project_detail_crawler.rb
+++ app/jobs/github_project_detail_crawler.rb
@@ -19,38 +19,45 @@ class GithubProjectDetailCrawler < Base
     # マルチプロセスで詳細情報を収集
     Parallel.each(targets, in_processes: Settings.detail_crawler_process_count) { |target|
       ActiveRecord::Base.connection_pool.with_connection do
-        # ブランチ情報
-        tree_results = fetch_projects_detail_branches_by_project_id(target.github_item_id)
-        save_project_detail_branches(target.id, tree_results)
+        begin
+          # ブランチ情報
+          tree_results = fetch_projects_detail_branches_by_project_id(target.github_item_id)
+          save_project_detail_branches(target.id, tree_results)
 
-        # タグ情報
-        tag_results = fetch_projects_detail_tags_by_project_id(target.github_item_id)
-        save_project_detail_tags(target.id, tag_results)
+          # タグ情報
+          tag_results = fetch_projects_detail_tags_by_project_id(target.github_item_id)
+          save_project_detail_tags(target.id, tag_results)
 
-        # ツリー情報から解析対象のファイル取得
-        tree_results = fetch_projects_detail_trees_by_project_id_and_sha(
-          target.github_item_id,
-          InputBranch.where(
-            input_project_id: target.id,
-            name: 'master'
-          ).first
-          .try(:sha)
-        )
-        save_project_detail_trees(target.id, tree_results)
+          # ツリー情報から解析対象のファイル取得
+          tree_results = fetch_projects_detail_trees_by_project_id_and_sha(
+            target.github_item_id,
+            InputBranch.where(
+              input_project_id: target.id,
+              name: 'master'
+            ).first
+            .try(:sha)
+          )
+          save_project_detail_trees(target.id, tree_results)
 
-        # 週間コミット情報
-        fetch_and_save_project_detail_weekly_commit_counts(
-          target.id,
-          target.github_item_id,
-          target.github_updated_at
-        )
+          # 週間コミット情報
+          fetch_and_save_project_detail_weekly_commit_counts(
+            target.id,
+            target.github_item_id,
+            target.github_updated_at
+          )
 
-        # コンテンツ
-        is_success = fetch_and_save_project_detail_contents(target.id)
+          # コンテンツ
+          is_success = fetch_and_save_project_detail_contents(target.id)
 
-        target.attributes = {
-          crawl_status: CrawlStatus::DONE
-        }
+          target.attributes = {
+            crawl_status: CrawlStatus::DONE
+          }
+
+        rescue
+          target.attributes = {
+            crawl_status: CrawlStatus::ERROR
+          }
+        end
         target.save!
       end
     }
@@ -94,6 +101,7 @@ class GithubProjectDetailCrawler < Base
         file_type: result.type,
         sha: result.sha,
         url: result.url,
+        size: result.size,
         input_project_id: target_id
       )
       pj.save!
