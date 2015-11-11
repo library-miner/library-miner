72fa950f778b81dac5750652b68ac973d192681a
r_nishio <quick.xp@me.com>
2015-11-11 21:48:53 +0900

    route api namespace 定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :homes, only: [:index]
+
+  namespace :api, defaults: { format: :json } do
+    resources :admin,only: [] do
+      collection do
+      end
+    end
+
+  end
+
+
 end
