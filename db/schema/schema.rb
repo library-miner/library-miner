92923c69a24799dfdcac131bff07ab788afd1eb4
r_nishio <quick.xp@me.com>
2015-11-14 23:05:39 +0900

    delayed job db schema

--- db/schema/schema.rb
+++ db/schema/schema.rb
@@ -240,6 +240,24 @@ create_table 'library_relation_errors', collate: 'utf8_bin', comment: 'プロジ
   t.datetime :updated_at
 end
 
+create_table :delayed_jobs, comment: "Delayed Job" do |t|
+  t.int :id, primary_key: true, extra: 'auto_increment'
+  t.int :priority, default: 0, null: false
+  t.int :attempts, default: 0, null: false
+  t.text :handler
+  t.text :last_error, null: true
+  t.datetime :run_at, null: true
+  t.datetime :locked_at, null: true
+  t.datetime :failed_at, null: true
+  t.varchar :locked_by, null: true
+  t.varchar :queue, null: true
+
+  t.datetime :created_at, null: true, comment: '作成日時'
+  t.datetime :updated_at, null: true, comment: '更新日時'
+
+  t.index [:priority, :run_at], name: "delayed_jobs_priority"
+end
+
 create_table 'schema_migrations', collate: 'utf8_bin', comment: '' do |t|
   t.varchar 'version'
 
