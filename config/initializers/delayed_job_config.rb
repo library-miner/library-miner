98cef397d08339e0ba8d6f1179773bf42481259f
r_nishio <quick.xp@me.com>
2015-12-19 12:33:16 +0900

    ジョブ実行間隔修正

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,4 +1,4 @@
 Delayed::Worker.destroy_failed_jobs = false # 失敗したジョブをDBから削除しない=false
-Delayed::Worker.sleep_delay = 60 # 実行ジョブがない場合に次回実行までのSleep時間（秒）
+Delayed::Worker.sleep_delay = 10 # 実行ジョブがない場合に次回実行までのSleep時間（秒）
 Delayed::Worker.max_attempts = 5 # リトライ回数
 Delayed::Worker.max_run_time = 60.minutes # 最大実行時間
