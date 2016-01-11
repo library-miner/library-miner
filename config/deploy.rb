5145543f3d0084b76f480025ce5dbd6ebd992f4d
r_nishio <quick.xp@me.com>
2016-01-11 16:07:48 +0900

    bower capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,5 +76,16 @@ namespace :deploy do
 
   before :started, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
-
 end
+
+namespace :bower do
+  desc 'Install bower'
+  task :install do
+    on roles(:web) do
+      within release_path do
+        execute :rake, 'bower:install CI=true'
+      end
+    end
+  end
+end
+before 'deploy:compile_assets', 'bower:install'
