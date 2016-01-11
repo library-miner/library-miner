0ba6281fa504e76a1773a9de86a7292b732509ad
r_nishio <quick.xp@me.com>
2016-01-11 22:21:25 +0900

    shell 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :pty, true
 
 # Shared
 set :linked_files, %w{config/database.yml config/secrets.yml config/settings.yml .env}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # RVM
 set :rvm_type, :system
