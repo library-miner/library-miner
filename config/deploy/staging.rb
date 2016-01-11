4b133f4efdb3b12677b0a579cd20163c6268b784
r_nishio <quick.xp@me.com>
2016-01-11 18:57:34 +0900

    test

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
-set :branch, 'master'
+set :branch, 'feature/20160105'
 set :rails_env, 'production'
 
 role :app, %w{miner@library-miner-db-local}
