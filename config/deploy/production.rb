dbabe53f8179a9742365ed5e60e623831e99efef
r_nishio <quick.xp@me.com>
2016-01-24 16:59:26 +0900

    capistrano 本番設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,18 +1,23 @@
-set :stage, :production
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
 set :branch, 'master'
+set :rails_env, 'production'
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{miner@library-miner-db miner@library-miner-monitor}
+role :web, %w{miner@library-miner-db}
+role :db,  %w{miner@library-miner-db}
 
+set :ssh_options, {
+  keys: [File.expand_path('~/.ssh/miner/id_rsa')],
+  forward_agent: true
+}
 
 # Extended Server Syntax
 # ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
