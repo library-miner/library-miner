96d284ef743522e83673ae699017bf06211a7544
r_nishio <quick.xp@me.com>
2016-03-13 13:57:59 +0900

    Travis CI WebMock使用時のエラー回避

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,11 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  # whitelist api.github.com so test coverage can be reported
+  config.after(:suite) do
+    WebMock.disable_net_connect!(:allow => 'api.github.com')
+  end
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
