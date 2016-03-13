ca7c6cb6329eb575b1b3a97235b9fd9505589347
r_nishio <quick.xp@me.com>
2016-03-13 14:10:00 +0900

    Travis CI WebMock使用時のエラー回避

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,9 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  # whitelist api.github.com so test coverage can be reported
+  # whitelist codeclimate.com so test coverage can be reported
   config.after(:suite) do
-    WebMock.disable_net_connect!(:allow => 'api.github.com')
+    WebMock.disable_net_connect!(:allow => 'codeclimate.com')
   end
 
   config.mock_with :rspec do |mocks|
